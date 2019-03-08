0e0943300ca181b05cc4dc2e62793b7924fcc38c
Muthuri Munene <bryanmuthuri38@gmail.com>
2019-03-08 14:00:52 +0300

    Chore(SASS): Setup SASS styling pre-processor (#7)
    - Add node-sass
    
    	- Change css to scss
    
    	-
    
    [Finishes #163388208]

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import 'semantic-ui-css/semantic.min.css';
+import './index.scss';
 import App from './App';
 import reducers from './reducers';
 
